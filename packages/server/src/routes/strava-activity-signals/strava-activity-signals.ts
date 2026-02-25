76c5a20be2308d09db3e58dab8e54d7db345022a
balovbohdan <balovbohdan@gmail.com>
2026-02-25 15:05:30 +0200

    id:47 feat(signals-package): used:

--- packages/server/src/routes/strava-activity-signals/strava-activity-signals.ts
+++ packages/server/src/routes/strava-activity-signals/strava-activity-signals.ts
@@ -1,5 +1,5 @@
 import getStravaActivitySignals from '@torqlab/get-strava-activity-signals';
-import type { StravaActivity } from '@torqlab/get-strava-activity-signals';
+import type { StravaActivity } from '@torq/strava-api';
 import checkForbiddenContent from '@torq/check-forbidden-content';
 
 /**
